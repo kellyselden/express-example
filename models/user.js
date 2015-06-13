453fb8bb01d6bf91fc7aceeae77c760cfb5b501d
Kelly Selden <kellyselden@gmail.com>
2015-06-13 11:39:37 -0400

    adding files

---
+++ models/user.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+ 
+module.exports = mongoose.model('User', {
+  name: String,
+  email: String,
+  password: String,
+  facebookId: String,
+  githubId: String
+});
---
+++ strategies/facebook.js
@@ -0,0 +1,3 @@
+module.exports = function(passport) {
+  require('./oauth2')(passport, 'facebook');
+};
